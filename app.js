6f7c56e695d835f7d7083708b9cc1622596dec54
Brandon <itzbrandonlee@yahoo.com>
2023-08-12 17:04:27 -0700

    files retrieved with links to folder

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const respond = require('./lib/respond.js');
+const port = process.env.port || 3000;
+const server = http.createServer(respond);
+
+server.listen(port, () => {
+    console.log(`listening to port: ${port}`);
+});
